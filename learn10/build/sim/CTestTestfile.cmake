f6c5eed1b0115fd26d7e709b4c2b6702dde9fea7
egod1537 <egod1537@gmail.com>
2026-04-30 18:00:43 +0900

    learn: 10

---
+++ learn10/build/sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yang/proj/ros-prac/learn10/src/sim
+# Build directory: /home/yang/proj/ros-prac/learn10/build/sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
