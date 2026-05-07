843fe921a235109917446fb2f43cc282614f331b
egod1537 <egod1537@gmail.com>
2026-05-07 10:56:29 +0900

    learn: 13

---
+++ learn13/src/main.cpp
@@ -0,0 +1,64 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "implot.h"
+#include "mcl.hpp"
+#include "sim/sim_config.hpp"
+#include "sim/sim_view.hpp"
+
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+
+  if (!glfwInit()) {
+    std::fprintf(stderr, "glfwInit failed\n");
+    return 1;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *win =
+      glfwCreateWindow(1280, 720, "MCL Localization 2D", nullptr, nullptr);
+  if (!win) {
+    std::fprintf(stderr, "glfwCreateWindow failed\n");
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(win);
+  glfwSwapInterval(1);
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImPlot::CreateContext();
+  ImGui_ImplGlfw_InitForOpenGL(win, true);
+  ImGui_ImplOpenGL3_Init("#version 330");
+
+  SimConfig config;
+  sim_config::load(config);
+  sim_config::sanitize(config);
+
+  Sim2D sim;
+  sim.init(config);
+
+  MCL mcl(0.05, 0.02, 0.1, 0.05, /*seed=*/42);
+  mcl.known_map.assign(sim.true_landmarks.begin(), sim.true_landmarks.end());
+  mcl.init_gaussian(/*M=*/500, 0.0, 0.0, 0.0, 0.1);
+
+  SimView view(win, sim, mcl, config);
+  while (!glfwWindowShouldClose(win))
+    view.render();
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImPlot::DestroyContext();
+  ImGui::DestroyContext();
+  glfwDestroyWindow(win);
+  glfwTerminate();
+
+  return 0;
+}
---
+++ learn13/src/sim/sim2d.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+#include <random>
+#include <vector>
+
+struct Pose2D {
+  double x = 0.0;
+  double y = 0.0;
+  double theta = 0.0;
+};
+
+struct Observation {
+  int id = -1;
+  double r = 0.0;
+  double phi = 0.0;
+};
+
+struct SensorConfig {
+  double max_range = 7.0;
+  double fov_half = 1.0471975511965976;
+  double sigma_r = 0.05;
+  double sigma_phi = 0.03;
+};
+
+struct MotionConfig {
+  double sigma_v = 0.03;
+  double sigma_w = 0.02;
+};
+
+struct SimConfig {
+  double sigma_v = 0.03;
+  double sigma_w = 0.02;
+
+  double max_range = 7.0;
+  double fov_half = 1.0471975511965976;
+  double sigma_r = 0.05;
+  double sigma_phi = 0.03;
+
+  int landmark_count = 6;
+  double landmark_min_x = -5.0;
+  double landmark_max_x = 5.0;
+  double landmark_min_y = -5.0;
+  double landmark_max_y = 5.0;
+};
+
+class Sim2D {
+public:
+  Sim2D();
+
+  void init(const SimConfig &config);
+  void step(double dist, double dtheta);
+  std::vector<Observation> measure();
+
+  void add_true_landmark(double x, double y);
+  bool remove_true_landmark_near(double x, double y, double radius);
+
+  Pose2D true_pose;
+  std::vector<Eigen::Vector2d> true_landmarks;
+  std::vector<Pose2D> trajectory_true;
+  SensorConfig sensor;
+  MotionConfig motion;
+
+private:
+  std::mt19937 rng_;
+};
